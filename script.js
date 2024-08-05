81dc69f6ce02e70907e0bf5d388d356392c9d8ec
Nazariy2 <nazrud23@gmail.com>
2024-08-05 12:54:56 +0200

    a

--- script.js
+++ script.js
@@ -1,9 +1,63 @@
-let counter = 0;
+// let counter = 0;
 
 
 
-while( counter <= 10) {
-    console.log('counter: ', counter)
-    counter += 1;
-}
+// while( counter <= 10) {
+//     console.log('counter: ', counter)
+//     counter += 1;
+// }
 
+
+// for (let i = 2; i <= 20; i++) {
+//     if (i % 2 !== 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`7 x ${i} = ${7 * i}`);
+// }
+
+
+// let number = [1, 2, 3, 4, 5];
+// let i = 0;
+
+
+// while (i < number.length) {
+//     console.log(number[i]);
+//     i++;
+// }
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for( let i = 0; i < numbers.length; i++) {
+//     if(numbers[i] === 7) {
+//         break;
+//     }
+//     console.log(numbers[i]);
+// }
+
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const n = 6;
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] >= n) {
+//         break;
+//     }
+//     console.log(numbers[i])
+// }
+
+// let i = 1;
+
+// while (i <= 20) {
+  
+//     if (i % 3 === 0) {
+//       i++;
+//       continue;
+//     }
+
+//     console.log(i);
+//     i++;
+//   }
